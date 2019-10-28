98c960fc06fc1512c668537a8ad9572545f5d92c
kingston-csj <definemyself@163.com>
2019-10-28 22:57:02 +0800

    修改密码界面

--- webapp/src/components/UserSetting.vue
+++ webapp/src/components/UserSetting.vue
@@ -1,11 +1,26 @@
 <template>
+ <div>
+   <el-select
+      v-model="chanelCode"
+      clearable>
+      <el-option
+       v-for="item in children"
+        :key="item"
+       :label="item"
+       :value="item"
+     ></el-option>
+   </el-select>
 
-<el-form-item label="可选角色">
-    <el-select v-model="children" multiple placeholder="请选择">
-        <el-option v-for="item in children" :key="item" :label="item" :value="item" ></el-option>
-    </el-select> 
-</el-form-item>
 
+    <el-form :model="resetForm" status-icon :rules="resetFormRules" ref="resetForm" label-width="100px">
+      <el-form-item label="新密码" prop="newpwd">
+          <el-input type="password" v-model="resetForm.newpwd" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="renewpwd">
+          <el-input type="password" v-model="resetForm.renewpwd" auto-complete="off"></el-input>
+      </el-form-item>
+    </el-form>
+</div>
    
 </template>
 <script>
@@ -34,6 +49,26 @@
           }
         });
       },
+      validatePass(rule, value, callback)  {
+          if (!value) {
+            callback(new Error('请输入新密码'));
+          } else if (value.toString().length < 6 || value.toString().length > 18) {
+            callback(new Error('密码长度为6 - 18个字符'))
+          } else {
+            callback();
+          }
+        },
+        validatePass2(rule, value, callback) {
+            if (value === '') {
+            callback(new Error('请再次输入密码'));
+            } else if (value !== this.resetForm.newpwd) {
+            callback(new Error('两次输入密码不一致!'));
+            } else {
+            callback();
+            }
+        },
+             
+
       searchClick(){
         this.loading = true;
         this.loadUserList();
@@ -42,7 +77,20 @@
     data(){
       return {
         loading: false,
+        chanelCode:'',
         children:[],
+        resetForm: {
+            newpwd: '',
+            renewpwd: '',
+        },
+        resetFormRules: {
+            newpwd: [
+                { required: true, validator: validatePass, trigger: 'blur' }
+            ],
+            renewpwd: [
+                { required: true, validator: validatePass2, trigger: 'blur' }
+            ]
+        },  
       }
     }
   }
